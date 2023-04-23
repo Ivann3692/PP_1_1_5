8adc33518fdd82e36f94a02e5ecc5f54358fddc4
Artem <ivann3692gmail.com>
2023-04-23 21:37:56 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,39 +1,36 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 public class UserServiceImpl implements UserService {
+    UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
-    UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-        userDao.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
-    public void dropUsersTable()  {
-        userDao.dropUsersTable();
+    public void dropUsersTable() {
+        userDaoHibernate.dropUsersTable();
     }
-
     public void saveUser(String name, String lastName, byte age) {
-        userDao.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
         System.out.println("Добавлен в базу " + name + " " + lastName);
     }
-
-    public void removeUserById(long id)  {
-        userDao.removeUserById(id);
+    public void removeUserById(long id) {
+        userDaoHibernate.removeUserById(id);
     }
-
     public List<User> getAllUsers() {
-        List<User> users = userDao.getAllUsers();
+        List<User> users = userDaoHibernate.getAllUsers();
         for (User user : users) {
             System.out.println(user);
         }
         return users;
     }
-
-    public void cleanUsersTable()  {
-        userDao.cleanUsersTable();
+    public void cleanUsersTable() {
+        userDaoHibernate.cleanUsersTable();
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,24 +1,63 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
 public class Util {
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest2";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "MySQL";
-    public static Connection getConnection(){
-        Connection connection = null;
-        try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
-            System.out.println("Connection OK");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            System.out.println("Connection error");
+  //  private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+  //  private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest2";
+  //  private static final String DB_USERNAME = "root";
+  //  private static final String DB_PASSWORD = "MySQL";
+  //  public static Connection getConnection(){
+  //       Connection connection = null;
+  //      try {
+  //          Class.forName(DB_DRIVER);
+  //          connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+  //          System.out.println("Connection OK");
+  //      } catch (ClassNotFoundException | SQLException e) {
+  //          e.printStackTrace();
+  //          System.out.println("Connection error");
+  //      }
+  //      return connection;
+  //  }
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest2");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "MySQL");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return connection;
+        return sessionFactory;
     }
+
+
+
+
+
+
+
 
 }
